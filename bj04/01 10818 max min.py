ea19857869f6f387d919b18fdcb6bb45bcc021e2
Heyden <nhm6947@gmail.com>
2022-04-15 23:33:51 +0900

    Study: 백준 4단계 01

---
+++ bj04/01 10818 max min.py
@@ -0,0 +1,44 @@
+'''
+문제
+N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+출력
+첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+
+예제 입력 1 
+5
+20 10 35 30 7
+
+예제 출력 1 
+7 35
+'''
+# 방법1. max, min 함수 활용
+N = int(input())
+data = list(map(int, input().split()))
+
+# 1-1. 시간은 1-2.보다 1초 빠르나 메모리가 조금 더 필요함
+print(min(data), max(data))
+
+# 1-2. 시간은 1-1.보다 1초 느리나 메모리가 조금 덜 필요함
+results = [min(data), max(data)]
+print(' '.join(map(str, results)))
+
+
+# 방법2. 1차원 배열 활용하기
+N = int(input())
+data = list(map(int, input().split()))
+minNum = data[0]
+maxNum = data[0]
+
+for i in range(len(data)):
+    # 최솟값 구하기
+    if minNum > data[i]:
+        minNum = data[i]
+    # 최댓값 구하기
+    if maxNum < data[i]:
+        maxNum = data[i]
+   
+print(minNum, maxNum)
